b8e22a809728cc184b815853e3da3ef53e65c908
Antti Matikainen <antti.matikainen@sievo.com>
2018-12-14 15:29:35 +0200

    filter stream

--- src/reddit-snoo.js
+++ src/reddit-snoo.js
@@ -19,12 +19,56 @@ const streamOpts = {
 const comments = client.CommentStream(streamOpts);
 
 // On comment, perform whatever logic you want to do
-comments.on('comment', comment => {
-  console.log(comment);
-  const user = r.getUser(comment.author.name);
+comments.on('comment', c => {
+  const comment = {
+    subreddit_name_prefixed: c.subreddit_name_prefixed,
+    link_title: c.link_title,
+    link_author: c.link_author,
+    link_permalink: c.link_permalink,
+    link_url: c.link_url,
+    author: c.author.name,
+    author_fullname: c.author_fullname,
+    body: c.body,
+    score: c.score,
+    created_utc: c.created_utc,
+    num_comments: c.num_comments,
+  };
+
+  console.log(chalk.blue('Comment from stream'), comment);
+  const user = r.getUser(comment.author);
   console.log(chalk.green('User'), user);
   // user.getOverview().then(o => {
   //   console.log(chalk.green('User.Overview', JSON.stringify(o)));
   // });
-  user.getTrophies().then(console.log);
+  user.getComments().then(c => {
+    const comments = c.map(x => {
+      return {
+        body: x.body,
+        link_permalink: x.link_permalink,
+        ups: x.ups,
+        downs: x.downs,
+        score: x.score,
+        subreddit: x.subreddit_name_prefixed,
+        likes: x.likes,
+        num_reports: x.num_reports,
+        link_url: x.link_url,
+      };
+    });
+    console.log(chalk.green(`User ${comment.author} comments`), comments);
+  });
 });
+
+// function x() {
+//   return new Promise(resolve => {
+//     setTimeout(c => {
+//       resolve('foo');
+//     }, 500);
+//   });
+// }
+
+// function main() {
+//   x().then(x => {
+//     return console.log(x);
+//   });
+// }
+// main();
